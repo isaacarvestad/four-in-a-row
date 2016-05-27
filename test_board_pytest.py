721ead236273a6bddd02cd12c488c713e1b21236
Isaac Arvestad <isaac.arvestad@gmail.com>
2016-05-27 13:04:12 +0200

    Add Board constructor test.

--- test_board_pytest.py
+++ test_board_pytest.py
@@ -1,5 +1,16 @@
 from board import Board
 
+def test_constructor():
+    board = Board(0,0)
+    assert board.boardMatrix.size == 0
+    assert board.columns == 0
+    assert board.rows == 0
+
+    board = Board(5,5)
+    assert board.boardMatrix.size == 25
+    assert board.columns == 5
+    assert board.rows == 5
+
 def test_addPiece():
     board = Board(5,5)
     board.addPiece(0, 1)
