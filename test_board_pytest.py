5eacf7851154735a3580caeb6421bcefc0608d6d
Isaac Arvestad <isaac.arvestad@gmail.com>
2016-05-27 12:57:46 +0200

    Remove print comment. Fix index of matrix check.

--- test_board_pytest.py
+++ test_board_pytest.py
@@ -1,8 +1,6 @@
 from board import Board
 
 def test_addPiece():
-    print("Testing adding a piece.")
-    
     board = Board(5,5)
     board.addPiece(0, 1)
-    assert board.boardMatrix.item(0,4) == 1
+    assert board.boardMatrix.item((4,0)) == 1
