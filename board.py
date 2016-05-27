0687ebfc3127b1d69c6d4cb79e4f9751554da9d2
Isaac Arvestad <isaac.arvestad@gmail.com>
2016-05-27 11:57:40 +0200

    Add Board class for representing board state.

---
+++ board.py
@@ -0,0 +1,13 @@
+"""
+Board represents a four in a row game board.
+
+Author: Isaac Arvestad
+"""
+class Board:
+    """
+    Initializes the game with a certain number of rows
+    and columns.
+    """
+    def __init(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
