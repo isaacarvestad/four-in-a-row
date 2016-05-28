c87a71035782da3a9f9b26c9fb6a30ce42855913
Isaac Arvestad <isaac.arvestad@gmail.com>
2016-05-28 10:59:48 +0200

    Change from camelcase to underscores.

--- board.py
+++ board.py
@@ -13,26 +13,26 @@ class Board:
     def __init__(self, rows, columns):
         self.rows = rows
         self.columns = columns
-        self.boardMatrix = numpy.zeros((rows, columns))
+        self.matrix = numpy.zeros((rows, columns))
 
     """
     Attempts to add a piece to a certain column. If the column is 
     full the move is illegal and false is returned, otherwise true 
     is returned.
     """
-    def addPiece(self, column, value):
+    def add_piece(self, column, value):
         "Check if column is full."
-        if self.boardMatrix.item(0,column) != 0:
+        if self.matrix.item(0,column) != 0:
             return False
 
         "Place piece."
         for y in range(self.rows):
             if y == self.rows - 1: # Reached bottom
-                self.boardMatrix.itemset((y, column), value)
+                self.matrix.itemset((y, column), value)
                 break
-            elif self.boardMatrix.item(y + 1, column) == 0: # Next row is also empty
+            elif self.matrix.item(y + 1, column) == 0: # Next row is also empty
                 continue
             else: # Next row is not empty
-                self.boardMatrix.itemset((y, column), value)
+                self.matrix.itemset((y, column), value)
                 break
         return True
